82a603a2021de841deacb335b7278a1ab949b810
Christian <xtianluz@gmail.com>
2021-10-06 23:29:51 +0800

    lates update

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = 'https://secure-shelf-79053.herokuapp.com/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
